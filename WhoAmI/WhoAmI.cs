e06fd45d067921249c4b38917239e60a23ddc377
Real-Gecko <alexandrbezenkov@gmail.com>
2016-07-10 16:44:08 +0600

    Version 1.0.1 Label is hidden while all GUI is hidden (F2 by default) Label is hidden after four seconds after appearing Label is not taking full screen width

--- WhoAmI/WhoAmI.cs
+++ WhoAmI/WhoAmI.cs
@@ -6,51 +6,81 @@ namespace WhoAmI
 	[KSPAddon(KSPAddon.Startup.Flight, false)]
 	public class WhoAmI: MonoBehaviour
 	{
-		private GUIStyle LabelStyle;
+		private GUIStyle labelStyle;
 		private string InfoText;
 		private Texture2D Background;
+		private bool isVisible = true;
+		private bool isActive = true;
+		private DateTime activatedAt = DateTime.Now;
 
 		void Awake() {
 			// Label background
 			Background = new Texture2D (1, 1);
 			Background.SetPixel(0, 0, Color.white);
 			// Label style
-			LabelStyle = new GUIStyle ();
-			LabelStyle.stretchWidth = true;
-			LabelStyle.stretchHeight = true;
-			LabelStyle.alignment = TextAnchor.MiddleCenter;
-			LabelStyle.fontSize = (Screen.height/25);
-			LabelStyle.fontStyle = FontStyle.Bold;
-			LabelStyle.normal.textColor = Color.black;
-			LabelStyle.normal.background = Background;
+			labelStyle = new GUIStyle ();
+			labelStyle.stretchWidth = true;
+			labelStyle.stretchHeight = true;
+			labelStyle.alignment = TextAnchor.MiddleCenter;
+			labelStyle.fontSize = (Screen.height/30);
+			labelStyle.fontStyle = FontStyle.Bold;
+			labelStyle.normal.textColor = Color.black;
+			labelStyle.normal.background = Background;
+			GameEvents.onHideUI.Add (onHideUI);
+			GameEvents.onShowUI.Add (onShowUI);
+			GameEvents.OnIVACameraKerbalChange.Add (OnIVACameraKerbalChange);
+			GameEvents.OnCameraChange.Add (OnCameraChange);
 		}
 
 		void OnDestroy()
 		{
 			Destroy (Background);
+			GameEvents.onHideUI.Remove (onHideUI);
+			GameEvents.onShowUI.Remove (onShowUI);
+			GameEvents.OnIVACameraKerbalChange.Remove (OnIVACameraKerbalChange);
+			GameEvents.OnCameraChange.Remove (OnCameraChange);
+		}
+
+		private void onHideUI() {
+			isVisible = false;
+		}
+
+		private void onShowUI() {
+			isVisible = true;
+		}
+
+		private void OnCameraChange(CameraManager.CameraMode cameraMode) {
+			if (cameraMode == CameraManager.CameraMode.IVA) {
+				isActive = true;
+				activatedAt = DateTime.Now;
+			} else
+				isActive = false;
+		}
+
+		private void OnIVACameraKerbalChange(Kerbal kerbal) {
+			activatedAt = DateTime.Now;
 		}
 
 		private void OnGUI() {
-			if (CameraManager.Instance.currentCameraMode == CameraManager.CameraMode.IVA ||
-				CameraManager.Instance.currentCameraMode == CameraManager.CameraMode.Internal)
-			{
+			if (isActive && isVisible && activatedAt.AddSeconds(4) > DateTime.Now) {
 				Kerbal kerbinaut = CameraManager.Instance.IVACameraActiveKerbal;
 				InfoText = String.Format (
 					"{0} - {1} ({2})",
 					kerbinaut.name,
 					kerbinaut.protoCrewMember.experienceTrait.TypeName,
-					kerbinaut.InPart.name
+					kerbinaut.InPart.partInfo.title
 				);
+				Vector2 labelSize = labelStyle.CalcSize (new GUIContent(InfoText));
 				GUILayout.BeginArea (
 					new Rect (
-						0,
-						Screen.height - Screen.height/20,
-						Screen.width,
-						Screen.height/20
+						(Screen.width - labelSize.x - 10)/2,
+						Screen.height - Screen.height / 25 - 10,
+						labelSize.x + 10,
+						Screen.height / 25
 					),
-					LabelStyle
+					labelStyle
 				);
-				GUILayout.Label (InfoText, LabelStyle);
+				GUILayout.Label (InfoText, labelStyle);
 				GUILayout.EndArea ();
 			}
 		}
